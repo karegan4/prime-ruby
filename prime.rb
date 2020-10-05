1f15dcbf2455c536d8e73d20ff380bc2bb7d4324
Kaitlin Regan <karegan@gmail.com>
2020-10-05 10:42:44 -0400

    done

--- prime.rb
+++ prime.rb
@@ -1,1 +1,9 @@
 # Add  code here!
+require 'pry'
+
+def prime?(integer)
+    return false if integer <= 1                              #Returns false if integer is less than or equal to 1
+    (2..Math.sqrt(integer)).none? {|i| (integer % i).zero?}   #Returns square root of integers 2...
+end                                                           #.none checks if none of the integers return square roots
+                                                              #% operator divides lefthand operand by righthand operand and returns remainder
+                                                              
